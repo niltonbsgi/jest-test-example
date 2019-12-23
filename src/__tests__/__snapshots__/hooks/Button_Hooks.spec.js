a58d063891687f8c89bee0da6e59d31aec12f84b
Nilton Rodrigues <nilton.rodrigues@programmers.com.br>
2019-12-23 14:39:43 -0300

    ...

---
+++ src/__tests__/__snapshots__/hooks/Button_Hooks.spec.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { create, act } from "react-test-renderer";
+
+function Button_Hooks(props) {
+  const [text, setText] = useState("");
+  function handleClick() {
+    setText("PROCEED TO CHECKOUT");
+  }
+  return <button onClick={handleClick}>{text || props.text}</button>;
+}
+
+describe("Button component", () => {
+  test("it shows the expected text when clicked", () => {
+    let component;
+    act(() => component = create(<Button_Hooks text="SUBSCRIBE TO BASIC" />));
+    const instance = component.root;
+    const button = instance.findByType("button");
+    act(() => button.props.onClick());
+    expect(button.props.children).toBe("PROCEED TO CHECKOUT");
+  });
+});
